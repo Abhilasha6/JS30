59fb3692d278702b7d26bb20af44b2a78c86ed5b
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2023-09-05 12:06:10 +0530

    sort & reduce exercise

--- Array Cardio Day 1/script.js
+++ Array Cardio Day 1/script.js
@@ -102,3 +102,38 @@ const links = category.querySelectorAll("a");
 const de = links
   .map((link) => link.textContent)
   .filter((streetName) => streetName.includes("de"));
+
+//sort by people last name
+const alpha = people.sort(function (lastone, nextone) {
+  const [alast, afirst] = lastone.split(", ");
+  const [blast, bfirst] = nextone.split(", ");
+  return alast > blast ? 1 : -1;
+});
+console.log(alpha);
+
+//sum up the instances of each of these using reduce
+const data = [
+  "car",
+  "car",
+  "truck",
+  "truck",
+  "bike",
+  "walk",
+  "car",
+  "van",
+  "bike",
+  "walk",
+  "car",
+  "van",
+  "car",
+  "truck",
+  "pogostick",
+];
+const transportation = data.reduce(function (obj, item) {
+  console.log(item);
+
+  if (!obj[item]) obj[item] = 0;
+
+  obj[item]++;
+  return obj;
+}, {});
