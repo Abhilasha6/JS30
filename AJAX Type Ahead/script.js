2da64f2c5715654d784530bb624df0dc0859aab2
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2023-10-07 20:57:58 +0530

    added comments to describe the functionalities

--- AJAX Type Ahead/script.js
+++ AJAX Type Ahead/script.js
@@ -9,7 +9,7 @@ fetch(endpoint) // returns a promise
 function findMatches(wordToMatch, cities) {
 return cities.filter(place =>{
     //find any related match if found
-    const regex = new RegExp(wordToMatch,'gi'); // look  gor global insensitive match of input city/ state
+    const regex = new RegExp(wordToMatch,'gi'); // look  for g=global(looks in the entire string for any matches) i = case-insensitive match of input city/ state
     return place.city.match(regex) || place.state.match(regex);
 });
 }
@@ -22,8 +22,8 @@ function displayMatches(){
     const matchArray = findMatches(this.value, cities);
     const html = matchArray.map( place => {
         const regex = new RegExp(this.value, 'gi');
-        const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
-        const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
+        const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`); //higlight the part of city name matching the regex
+        const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`); //higlight the part of state name matching the regex
 
         return `
         <li>
